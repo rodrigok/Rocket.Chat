1d18b7a5e82ea46339493a63cd3d08e62d3fc563
Christopher Price <cprice@priceconsultinggroupinc.com>
2020-01-24 11:53:18 -0500

    added API route for resetRoomAvatar; removed a version of resetRoomAvatar that I put in the wrong place.

--- app/api/server/v1/users.js
+++ app/api/server/v1/users.js
@@ -250,6 +250,22 @@ API.v1.addRoute('users.resetAvatar', { authRequired: true }, {
 	},
 });
 
+API.v1.addRoute('users.resetRoomAvatar', { authRequired: true }, {
+	post() {
+		const user = this.getUserFromParams();
+
+		if (user._id === this.userId) {
+			Meteor.runAsUser(this.userId, () => Meteor.call('resetRoomAvatar'));
+		} else if (hasPermission(this.userId, 'edit-other-user-info')) {
+			Meteor.runAsUser(user._id, () => Meteor.call('resetRoomAvatar'));
+		} else {
+			return API.v1.unauthorized();
+		}
+
+		return API.v1.success();
+	},
+});
+
 API.v1.addRoute('users.setAvatar', { authRequired: true }, {
 	post() {
 		check(this.bodyParams, Match.ObjectIncluding({
